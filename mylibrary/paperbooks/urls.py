9963ff66a02f284687d3e1c8f3ff31cfa5d0545f
gshabunya1 <gshabunya1@users.noreply.github.com>
2023-06-02 16:07:25 +0300

    futh

--- mylibrary/paperbooks/urls.py
+++ mylibrary/paperbooks/urls.py
@@ -3,6 +3,10 @@ from django.urls import  path
 
 
 urlpatterns = [
+    path('register', views.RegisterFormView.as_view()),
+    path('login', views.LoginFormView.as_view()),
+    path('logout', views.LogoutView.as_view()),
+    path('password-change', views.PasswordChangeView.as_view()),
     path('', views.home, name="home"),
     path('paperbooks/', views.home, name="home"),
 ]
--- mylibrary/paperbooks/views.py
+++ mylibrary/paperbooks/views.py
@@ -2,9 +2,123 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# Базовый класс для обработки страниц с формами.
+from django.views.generic.edit import FormView
+# Спасибо django за готовую форму регистрации.
+from django.contrib.auth.forms import UserCreationForm
+# Спасибо django за готовую форму аутентификации
+from django.contrib.auth.forms import AuthenticationForm
+# Для смены пароля - форма
+from django.contrib.auth.forms import PasswordChangeForm
+# Функция для установки сессионного ключа.
+# По нему django будет определять,
+# выполнил ли вход пользователь.
+from django.contrib.auth import login
+# Для Log out с перенаправлением на главную
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.views.generic.base import View
+from django.contrib.auth import logout
 
+app_url = "/paperbooks/"
+
+class RegisterFormView(FormView):
+    # будем строить на основе
+    # встроенной в django формы регистрации
+    form_class = UserCreationForm
+    # Ссылка, на которую будет перенаправляться пользователь
+    # в случае успешной регистрации.
+    # В данном случае указана ссылка на
+    # страницу входа для зарегистрированных пользователей.
+    success_url = app_url + "login/"
+    # Шаблон, который будет использоваться
+    # при отображении представления.
+    template_name = "reg/register.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя,
+        # если данные в форму были введены корректно.
+        form.save()
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+
+class LoginFormView(FormView):
+    # будем строить на основе
+    # встроенной в django формы входа
+    form_class = AuthenticationForm
+    # Аналогично регистрации,
+    # только используем шаблон аутентификации.
+    template_name = "reg/login.html"
+    # В случае успеха перенаправим на главную.
+    success_url = app_url
+
+    def form_valid(self, form):
+        # Получаем объект пользователя
+        # на основе введённых в форму данных.
+        self.user = form.get_user()
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+class LogoutView(View):
+    def get(self, request):
+        # Выполняем выход для пользователя,
+        # запросившего данное представление.
+        logout(request)
+        # После чего перенаправляем пользователя на
+        # главную страницу.
+        return HttpResponseRedirect(app_url)
+
+# наше представление для смены пароля
+class PasswordChangeView(FormView):
+    # будем строить на основе
+    # встроенной в django формы смены пароля
+    form_class = PasswordChangeForm
+    template_name = 'reg/password_change_form.html'
+    # после смены пароля нужно снова входить
+    success_url = app_url + 'login/'
+
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        if self.request.method == 'POST':
+            kwargs['data'] = self.request.POST
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(PasswordChangeView, self).form_valid(form)
+
+# Create your views here.
+# главная страница 
 def home(request):
-    return render(request, "home.html")
+    message = None
+    if "message" in request.GET:
+        message = request.GET["message"]
+    # создание HTML-страницы по шаблону index.html
+    # с заданными параметрами latest_riddles и message
+    return render(
+        request,
+        "home.html",
+        {
+            "message": message
+        }
+    )
+
+def index(request):
+    message = None
+    if "message" in request.GET:
+        message = request.GET["message"]
+    # создание HTML-страницы по шаблону index.html
+    # с заданными параметрами latest_riddles и message
+    return render(
+        request,
+        "home.html",
+        {
+            "message": message
+        }
+    )
+
+
 
 
 
