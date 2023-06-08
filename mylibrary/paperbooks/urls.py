725d21981d09316aff4c4da4cf2c0813ab0f2e54
HelenaValkova <helenvalkova@toppanmerrillllc.com>
2023-06-08 11:21:20 +0300

    aaaa

--- mylibrary/paperbooks/urls.py
+++ mylibrary/paperbooks/urls.py
@@ -1,6 +1,5 @@
 from . import views
-from django.urls import  path
-
+from django.urls import  path, re_path
 
 urlpatterns = [
     path('register', views.RegisterFormView.as_view()),
@@ -8,5 +7,8 @@ urlpatterns = [
     path('logout', views.LogoutView.as_view()),
     path('password-change', views.PasswordChangeView.as_view()),
     path('', views.home, name="home"),
-    path('paperbooks/', views.home, name="home"),
+    path('paperbooks/', views.index, name="warehous"),
+    path('paperbooks/index/', views.index, name="index"),
+    path('index/', views.index, name="index"),
+    
 ]
